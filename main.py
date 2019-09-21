b4fb383177b11e2d62e58016891d52b276745aad
Apolexian <ivananikitin98@gmail.com>
2019-09-21 12:36:46 +0100

    added functions to work on the qtable

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from game import *
+
 from numpy import subtract
+import operator
+import random
 # Some variables that will be used for q learning
 # For now numbers are just approximations
 q_table = {}
 avail_actions = ['up', 'down', 'left', 'right']
 learning_rate = 0.85
-dicount_factor = 0.9
+discount_factor = 0.9
 random_rate = 0.05
 
 
@@ -18,3 +20,36 @@ def select_state(snake, food):
     tail_rel = tuple(subtract(tail_pos, head_pos))
     food_rel = tuple(subtract(food_pos, head_pos))
     return (food_rel, tail_rel)
+
+
+# Function to return the state and its action values
+# If state does not exist add it and set action values to 0
+def q_table_lookup(state):
+    if state in q_table.keys():
+        return q_table[state]
+    else:
+        q_table[state] = {'up': 0, 'down': 0, 'left': 0, 'right': 0}
+        return q_table[state]
+
+
+# Function to select best action from a given state
+# If all state are 0 return random action
+def select_action(state):
+    if all(value == 0 for value in state.values()):
+        best = random.choice(list(state))
+        return best
+    else:
+        best = max(state.items(), key=operator.itemgetter(1))[0]
+    return best
+
+
+# Function to update the state in the q table
+# use select action to estimate the optimal future value
+# calculate learned value by adding reward to discount * predicted
+# subtract the old value
+# then update q table by old value + learning rate*new value
+def q_table_update(state0, state1, reward, action):
+    q0 = q_table_lookup(state0)
+    q1 = q_table_lookup(state1)
+    new_val = reward + discount_factor * select_action(q1) - q0[action]
+    q_table[state0][action] = q0[action] + learning_rate * new_val
